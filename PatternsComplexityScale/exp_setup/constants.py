b50d85ac99508e8fba57d1247698801fff2e3f75
Oleksandr Kostin <oleksandrkostin2000@gmail.com>
2024-07-02 20:40:07 +0200

    fixing issue with generating multiple visible regions for a mask

--- PatternsComplexityScale/exp_setup/constants.py
+++ PatternsComplexityScale/exp_setup/constants.py
@@ -3,9 +3,9 @@ Author: Oleksandr Kostin
 """
 
 constants = {
-    'feedback_message_duration': 3000,       # milliseconds
-    'pattern_image_display_duration': 700,  # milliseconds
-    'number_image_display_duration': 200,   # milliseconds
+    'feedback_message_duration': 4000,       # milliseconds
+    'pattern_image_display_duration': 7000,  # milliseconds
+    'number_image_display_duration': 2000,   # milliseconds
     'mask_visibility_initial': 0.4,
     'mask_pieces_min': 1,
     'mask_pieces_max': 5,
